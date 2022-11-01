675cda7096a1a079ea29d65a06a4eea0fadaf17c
Dylan <cadotdylan7@gmail.com>
2022-11-01 23:32:51 +0100

    gestion du findBy

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -15,7 +15,7 @@ class AlbumController extends AbstractController
      */
     public function listealbums(AlbumRepository $repo): Response
     {
-        $albums=$repo->findAll();
+        $albums = $repo->findBy(['date'=>'2006'],['nom'=>'asc'],5);
         return $this->render('album/listeAlbums.html.twig', [
             'lesAlbums' => $albums
         ]);
